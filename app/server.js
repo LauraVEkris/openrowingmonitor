31ab138ae82e9a25a955de5650e1b596862d303b
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2024-12-02 12:59:12 +0100

    Fix Lint error

--- app/server.js
+++ app/server.js
@@ -102,7 +102,7 @@ sessionManager.on('metricsUpdate', (metrics) => {
 
 workoutUploader.on('authorizeStrava', (data, client) => {
   // ToDo: bring further in line with command handler structure to allow workoutUploader to send more commands
-  handleCommand ('authorizeStrava', data, client)
+  handleCommand('authorizeStrava', data, client)
 })
 
 const webServer = createWebServer(config)
