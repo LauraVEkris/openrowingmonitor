cef642d9babd77ba56830b4e8e6afeccba83226a
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2024-12-29 17:28:33 +0100

    Fixed Lint error

--- app/recorders/tcxRecorder.js
+++ app/recorders/tcxRecorder.js
@@ -342,7 +342,7 @@ export function createTCXRecorder (config) {
 
   async function createNotes (workout) {
     let VO2maxoutput
-  
+
     // VO2Max calculation
     const VO2maxResult = VO2max.result()
     if (VO2maxResult > 10 && VO2maxResult < 60) {
